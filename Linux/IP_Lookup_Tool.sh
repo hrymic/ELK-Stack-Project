564c1099e8817d23077e7c9a015e25703261ccd4
hrymic <84352315+hrymic@users.noreply.github.com>
2021-11-18 21:55:29 -0800

    Update IP_Lookup_Tool.sh

--- Linux/IP_Lookup_Tool.sh
+++ Linux/IP_Lookup_Tool.sh
@@ -4,21 +4,27 @@
  
  # Location can be determined using only the coordinates
 
- # isolcate the longitude and latitude 
+ # isolate the longitude and latitude 
      grep -i loc $1.txt > $1_loc.txt 
      awk -F" " '{print $2}' $1_loc.txt > $1_loc2.txt
      cat $1_loc2.txt | sed s/'"*",'// | sed s/'"'// > $1_coordinates.txt
 
  # remove extra location files
      rm $1_loc.txt $1_loc2.txt
+     
+ # grep org information
+     grep -i org $1.txt > $1_org.txt
 
  # save IP lookup info in a directory
-     echo "Creating directory $1"
-     mkdir $1 | mv $1* $1
+     echo "Creating directory. Type in organization name then hit Enter (Use _ for spaces)"
+     cat $1_org.txt
+     read organization
+     mkdir $organization
+     mv $1*.txt $organization
 
  # user input required to open Google Maps
      echo "Type the following coordinates then hit Enter"
-     cat $1/$1_coordinates.txt
+     cat $organization/$1_coordinates.txt
      read coordinates
-     echo "Looking up $coordinates"
+     echo "Looking up $organization at $coordinates"
      firefox https://www.google.com/maps/@"$coordinates",14z
